99d814114d967ac6e866e4d356ce11ac16ef92cb
Timothy Pokanai <pokanait@mcmaster.ca>
2025-05-19 12:29:16 -0400

    Created ShortUrl db model

---
+++ src/main/java/com/pokeaneye/urlshortener/model/ShortUrl.java
@@ -0,0 +1,34 @@
+package com.pokeaneye.urlshortener.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ShortUrl {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String originalUrl;
+
+    @Column(nullable = false, unique = true)
+    private String shortCode;
+
+    private LocalDateTime createdAt;
+}
